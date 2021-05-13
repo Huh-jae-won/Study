6f65a115cabd2d6a0eaab3e806530188c8b9e393
Jaewon <dfr9034@naver.com>
2021-05-14 00:00:59 +0900

    Chapter) Web MVC Development
     - 회원 웹 기능 추가
       홈 화면, 회원 등록, 회원 조회

--- Spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Spring/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,7 +1,14 @@
 package hello.hellospring.controller;
+
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller // controller선언 : 컨테이너에 담아줌
 public class MemberController {
@@ -16,4 +23,24 @@ public class MemberController {
         this.memberService = memberService;
     }
     // 직접 스프링빈 등록방식 -> SpringConfig파일에서 작성
+
+    @GetMapping("/members/new")
+    public String createForm(){
+        return "members/createMemberForm";
+    }
+    @PostMapping("/members/new")
+    public String create(MemberForm form){
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+        return "redirect:/"; // 다시 홈화면으로 돌려보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model){
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members);
+        return "members/memberList";
+    }
 }
