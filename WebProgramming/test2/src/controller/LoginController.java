0d3215d78fd8d9990f93184b6d9dd76f1e49718a
Huh-jae-won <dfr9034@naver.com>
2021-05-27 11:10:01 +0900

    login 구현
     - 로그인 메소드
     - 세션에 로그인 정보를 저장

--- WebProgramming/test2/src/controller/LoginController.java
+++ WebProgramming/test2/src/controller/LoginController.java
@@ -8,16 +8,60 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import dao.UserDAO;
+import model.UserDTO;
 
 @WebServlet("/login")
 public class LoginController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession hs = request.getSession();
+		String loginUser = (String)hs.getAttribute("loginUser");
+		if(loginUser!=null) {	
+			// 이미 로그인 -> 메인페이지로 이동하도록
+			response.sendRedirect("/main");
+			return;
+		}
 		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/login.jsp");
 		rd.forward(request, response);
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String id = request.getParameter("id");
+		String pw = request.getParameter("pw");
+		
+//		System.out.println(id+", "+pw);
+		
+		UserDTO uDTO = new UserDTO();
+		UserDAO uDAO = new UserDAO();
+		uDTO.setUserId(id);
+		uDTO.setUserPw(pw);
+		int ret = uDAO.doLogin(uDTO);
+		String msg = "로그인에 성공했습니다.";
+		String url = "/main";
+		switch (ret) {
+		case 1:		// 로그인 성공 -> 메인페이지로
+			// 로그인 정보를 세션에 저장 : 모든 페이지에서 사용할수있도록
+			HttpSession hs = request.getSession();
+			hs.setAttribute("loginUser", id);
+			break;
+		case -1:	// 로그인 실패 -> 다시 로그인페이지로
+			msg = "아이디 또는 패스워드가 일치하지 않습니다.";
+			url = "/login";
+			break;
+		default:	// 알수없는에러(0), 에러(-2)
+			msg = "DB 오류 발생!! 관리자에게 문의하세요.";
+			url = "/login";
+			break;
+		}
+		request.setAttribute("msg", msg);
+		request.setAttribute("url", url);
+		request.setAttribute("uDTO", uDTO);
+		
+		RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/common/commonMsg.jsp");
+		rd.forward(request, response);
 	}
 }
--- WebProgramming/test2/src/dao/UserDAO.java
+++ WebProgramming/test2/src/dao/UserDAO.java
@@ -2,12 +2,45 @@ package dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import connection.Conn;
 import model.UserDTO;
 
 public class UserDAO {
 	// DAO : Data Access Object
+	
+	// 로그인 메소드
+	public int doLogin(UserDTO param) {
+		// ret : 성공(1), 실패(-1), 알수없는에러(-2), 쿼리문실패(0)
+		int ret = 0;
+		Connection con = null;			// DB연결용
+		PreparedStatement ps = null;	// DB쿼리용
+		ResultSet rs = null;			// select 반환용
+		String sql = "SELECT * FROM user_tb WHERE user_id=? AND user_pw=PASSWORD(?)";
+
+		try {
+			con = Conn.getCon();
+			ps = con.prepareStatement(sql);	// 쿼리진행
+			ps.setString(1, param.getUserId());
+			ps.setString(2, param.getUserPw());
+			rs = ps.executeQuery();	// select 실행값 반환
+			if(rs.next()) {
+				ret = 1;	// 로그인 성공
+				// 세션에 로그인 정보를 저장
+			}else {
+				ret = -1;	// 로그인 실패
+			}
+		} catch (Exception e) {
+			ret = -2;
+			e.printStackTrace();
+		}			// DB연결
+		finally {
+			Conn.close(con, ps, rs);
+		}
+		return ret;
+	}
+	
 	// 회원가입 메소드
 	public int doJoin(UserDTO param) {
 		int ret = 0;
@@ -23,7 +56,7 @@ public class UserDAO {
 			ps.setString(2, param.getUserPw());
 			ret = ps.executeUpdate();	// 쿼리 실행
 		} catch (Exception e) {
-			ret = -1;
+			ret = -2;
 			e.printStackTrace();
 		}
 		finally {
